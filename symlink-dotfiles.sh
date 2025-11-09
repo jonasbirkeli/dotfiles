757c806ece716b628f5fca7b1e3a1c5bdc8d85ac
Jonas Birkeli <jonas.birkeli@icloud.com>
2025-11-09 08:56:28 +0100

    Create automatic symlink script with backup

---
+++ symlink-dotfiles.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# symlink-dotfiles.sh
+# Creates symlinks from all files/folders in ~/dotfiles to ~ (home directory)
+
+DOTFILES_DIR="$HOME/dotfiles"
+BACKUP_DIR="$HOME/dotfiles_backup_$(date +%Y%m%d%H%M%S)"
+
+# List of files/folders to symlink
+FILES=(
+    ".zshrc"
+    ".zprofile"
+    ".vimrc"
+    ".vim"
+    ".ssh"
+    ".vscode-oss"
+)
+
+echo "Backing up existing dotfiles to $BACKUP_DIR..."
+mkdir -p "$BACKUP_DIR"
+
+for file in "${FILES[@]}"; do
+    TARGET="$HOME/$file"
+    SOURCE="$DOTFILES_DIR/$file"
+
+    if [ -e "$TARGET" ] || [ -L "$TARGET" ]; then
+        echo "Backing up $TARGET"
+        mv "$TARGET" "$BACKUP_DIR/"
+    fi
+
+    echo "Creating symlink for $file"
+    ln -s "$SOURCE" "$TARGET"
+done
+
+echo "All symlinks created successfully."
+
